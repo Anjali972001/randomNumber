2777b1cd50b22943a192302f2421ace1df6faaec
Anjali <anjalikumari972001@gmail.com>
2022-09-12 00:34:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+prompt("what's your Name ?");
+prompt("what's your crush name?");
+
+
+var loveScore=Math.random()*100;
+loveScore=Math.floor(loveScore)+1;
+//console.log(loveScore);
+alert("Your love score is "+loveScore );

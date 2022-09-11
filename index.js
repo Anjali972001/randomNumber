e46026c214bb86a0c3fa21b808103c968b0e92cc
Anjali <anjalikumari972001@gmail.com>
2022-09-12 00:53:48 +0530

    new

--- index.js
+++ index.js
@@ -5,4 +5,14 @@ prompt("what's your crush name?");
 var loveScore=Math.random()*100;
 loveScore=Math.floor(loveScore)+1;
 //console.log(loveScore);
-alert("Your love score is "+loveScore );
+//alert("Your love score is "+loveScore );
+
+// conditional statements:
+if(loveScore>70)
+{
+   alert("your love score is "+loveScore+" % and your are suitable couple for each other")
+}
+else
+{
+    alert(loveScore+" % You can be just friends.")
+}
